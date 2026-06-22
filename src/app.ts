aace841cd04a3e33d526a6cb07a278a6ea923384
shahadot99999 <shahadot99999@gmail.com>
2026-06-22 19:09:55 +0600

    Registering First User With Prisma And Register API

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,84 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+import  httpStatus  from "http-status";
+import bcrypt from "bcryptjs";
 
+//import { config } from "dotenv";
 
 const app : Application = express();
 
+//middleware
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+
+
 app.get("/", (req:Request, res: Response)=>{
+   
     res.send("Hello World");
+})
+
+app.post("/api/users/register", async(req : Request, res: Response)=>{
+const {name, email, password, profilePhoto} = req.body;
+
+const isUserExist = await prisma.user.findUnique({
+    where : {email}
+})
+
+if(isUserExist){
+    throw new Error("User with this email already exists");
+}
+
+const hashedPassword = await bcrypt.hash(password, Number(config.bcrypt_salt_rounds));
+
+const createdUser = await prisma.user.create({
+    data: {
+        name,
+        email,
+        password: hashedPassword,
+
+    }
+});
+
+ await prisma.profile.create({
+    data:{
+
+        userId : createdUser.id,
+        profilePhoto
+    }
+})
+
+const user = await prisma.user.findUnique({
+    where:{
+        id: createdUser.id,
+        email : createdUser.email || email
+    },
+    omit: {
+    password: true
+    },
+    include: {
+       profile: true 
+    }
+})
+
+res.status(httpStatus.CREATED).json({ 
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message : "User registered successfully",
+
+    data: {
+        user
+    }
+});
 })
 
 export default app; 
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
 import app from "./app.js";
+import config from "./config/index.js";
 import { prisma } from "./lib/prisma.js";
 import "dotenv/config";
 
-const PORT = process.env.PORT ;
+// const PORT = process.env.PORT ;
+//const PORT = config.port;
+const PORT = 5000;
 
 async function main() {
     try {
-        //await prisma.$connect();
+        await prisma.$connect();
         console.log("Connect to the database successfully.");
         app.listen(PORT, ()=>{
             console.log(`Server is running on the port ${PORT}`);
         })
     } catch (error) {
         console.log("Error starting the server", error);
-        //await prisma.$disconnect();
+        await prisma.$disconnect();
         process.exit(1);
         
     }
